c26514aea4ff7017bb44572a5645a960ebcee559
Kevin Nguyen <k.nguyen.2@campus.tu-berlin.de>
2024-11-09 12:44:21 +0100

    Create config file for ride-pooling app

---
+++ src/main/java/com/dcaiti/mosaic/app/ridehailing/config/CAbstractRidePoolingVehicleApp.java
@@ -0,0 +1,7 @@
+package com.dcaiti.mosaic.app.ridehailing.config;
+
+public class CAbstractRidePoolingVehicleApp {
+    // The name of the server the AbstractRidePoolingServiceApp is
+    // running on.
+    public String serverName = "server_0";
+}
