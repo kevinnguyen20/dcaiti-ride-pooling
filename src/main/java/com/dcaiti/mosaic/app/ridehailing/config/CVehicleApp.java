55368015a95b7b5e4af9f5df75e43db5a0d1c020
Kevin Nguyen <k.nguyen.2@campus.tu-berlin.de>
2024-11-13 10:26:33 +0100

    Create config file for VehicleApp

---
+++ src/main/java/com/dcaiti/mosaic/app/ridehailing/config/CVehicleApp.java
@@ -0,0 +1,36 @@
+package com.dcaiti.mosaic.app.ridehailing.config;
+
+import org.eclipse.mosaic.lib.enums.VehicleStopMode;
+import org.eclipse.mosaic.lib.util.gson.TimeFieldAdapter;
+import org.eclipse.mosaic.rti.TIME;
+
+import com.google.gson.annotations.JsonAdapter;
+
+public class CVehicleApp {
+    
+    /**
+     * The usual time the vehicle will stop at a stop location
+     * before proceeding the journey.
+     */
+    @JsonAdapter(TimeFieldAdapter.NanoSeconds.class)
+    public long stopTime = 20 * TIME.SECOND;
+
+    /**
+     * The time the vehicle will wait at most at the stop position, if
+     * a wait time is given for the stop.
+     */
+    @JsonAdapter(TimeFieldAdapter.NanoSeconds.class)
+    public long maxStopTime = 2 * TIME.MINUTE;
+
+    /**
+     * Defines the mode for stopping. Use "PARK_ON_ROADSIDE" or "STOP".
+     */
+    public VehicleStopMode stopMode = VehicleStopMode.PARK_ON_ROADSIDE;
+
+    /**
+     * If set to true, the vehicle waits at the drop-off stop
+     * until the given time is reached, but it waits not more than
+     * what is configured in maxStopTime.
+     */
+    public boolean waitUntilDropOffTime = false;
+}
