88a540eb3c914c2aca94e876811b6f2135502eb5
Kevin Nguyen <k.nguyen.2@campus.tu-berlin.de>
2024-11-13 10:28:52 +0100

    Minor changes for ride-pooling

--- src/main/java/com/dcaiti/mosaic/app/ridehailing/server/VehicleStatus.java
+++ src/main/java/com/dcaiti/mosaic/app/ridehailing/server/VehicleStatus.java
@@ -1,10 +1,15 @@
 package com.dcaiti.mosaic.app.ridehailing.server;
 
 import org.eclipse.mosaic.lib.geo.GeoPoint;
+import org.eclipse.mosaic.lib.routing.CandidateRoute;
+
+import com.dcaiti.mosaic.app.ridehailing.vehicle.VehicleStop;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class VehicleStatus implements Serializable {
 
@@ -14,20 +19,22 @@ public class VehicleStatus implements Serializable {
 
     private final String vehicleId;
     private final GeoPoint currentPosition;
-    private List<Ride> listOfCurrentRides = new ArrayList<>();
-    private final Ride currentRide;
+    private List<Ride> rides = new ArrayList<>();
     private final double totalDistanceDriven;
+    private Queue<VehicleStop> stops = new LinkedList<>();
+    private Queue<CandidateRoute> routes = new LinkedList<>();
 
-    public VehicleStatus(String vehicleId, GeoPoint currentPosition, double distanceDriven, List<Ride> listOfCurrentRides, Ride currentRide) {
+    public VehicleStatus(String vehicleId, GeoPoint currentPosition, double distanceDriven, List<Ride> rides, Queue<VehicleStop> stops, Queue<CandidateRoute> routes) {
         this.vehicleId = vehicleId;
         this.currentPosition = currentPosition;
-        this.listOfCurrentRides = listOfCurrentRides;
-        this.currentRide = currentRide;
+        this.rides = rides;
+        this.stops = stops;
+        this.routes = routes;
         this.totalDistanceDriven = distanceDriven;
     }
 
     public boolean hasEnoughCapacity() {
-        return listOfCurrentRides.size() < VEHICLE_CAPACITY;
+        return rides.size() < VEHICLE_CAPACITY;
     }
 
     public String getVehicleId() {
@@ -38,15 +45,19 @@ public class VehicleStatus implements Serializable {
         return currentPosition;
     }
 
-    public List<Ride> getlistOfCurrentRides() {
-        return listOfCurrentRides;
-    }
-
-    public Ride getCurrentRide() {
-        return currentRide;
+    public List<Ride> getRides() {
+        return rides;
     }
 
     public double getTotalDistanceDriven() {
         return totalDistanceDriven;
+    }
+
+    public Queue<VehicleStop> getStops() {
+        return stops;
+    }
+
+    public Queue<CandidateRoute> getRoutes() {
+        return routes;
     }
 }
