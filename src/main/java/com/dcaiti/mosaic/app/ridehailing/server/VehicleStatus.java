35932d938f1f8c4f8de99dbf35cb72762b027ddd
Kevin Nguyen <k.nguyen.2@campus.tu-berlin.de>
2024-11-22 10:08:40 +0100

    Delete unnecessary instance vars

--- src/main/java/com/dcaiti/mosaic/app/ridehailing/server/VehicleStatus.java
+++ src/main/java/com/dcaiti/mosaic/app/ridehailing/server/VehicleStatus.java
@@ -1,15 +1,9 @@
 package com.dcaiti.mosaic.app.ridehailing.server;
 
 import org.eclipse.mosaic.lib.geo.GeoPoint;
-import org.eclipse.mosaic.lib.routing.CandidateRoute;
-
-import com.dcaiti.mosaic.app.ridehailing.vehicle.VehicleStop;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 
 public class VehicleStatus implements Serializable {
 
@@ -20,18 +14,11 @@ public class VehicleStatus implements Serializable {
     private final List<Ride> currentRides;
     private final double totalDistanceDriven;
     private int vehicleCapacity = 4;
-    
-    private List<Ride> rides = new ArrayList<>();
-    private Queue<VehicleStop> stops = new LinkedList<>();
-    private Queue<CandidateRoute> routes = new LinkedList<>();
 
-    public VehicleStatus(String vehicleId, GeoPoint currentPosition, List<Ride> currentRides, List<Ride> rides, Queue<VehicleStop> stops, Queue<CandidateRoute> routes, double distanceDriven) {
+    public VehicleStatus(String vehicleId, GeoPoint currentPosition, List<Ride> currentRides, double distanceDriven) {
         this.vehicleId = vehicleId;
         this.currentPosition = currentPosition;
         this.currentRides = currentRides;
-        this.rides = rides;
-        this.stops = stops;
-        this.routes = routes;
         this.totalDistanceDriven = distanceDriven;
     }
 
@@ -49,18 +36,6 @@ public class VehicleStatus implements Serializable {
 
     public List<Ride> getCurrentRides() {
         return currentRides;
-    }
-
-    public List<Ride> getRides() {
-        return rides;
-    }
-
-    public Queue<VehicleStop> getStops() {
-        return stops;
-    }
-    
-    public Queue<CandidateRoute> getRoutes() {
-        return routes;
     }
 
     public double getTotalDistanceDriven() {
