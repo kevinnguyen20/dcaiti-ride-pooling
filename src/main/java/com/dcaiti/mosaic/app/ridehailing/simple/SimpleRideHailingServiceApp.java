bdefe2aadd382542fa7f9793b150343c7fa3c7cb
Kevin Nguyen <k.nguyen.2@campus.tu-berlin.de>
2024-12-04 12:29:51 +0100

    Comment out unused classes

--- src/main/java/com/dcaiti/mosaic/app/ridehailing/simple/SimpleRideHailingServiceApp.java
+++ src/main/java/com/dcaiti/mosaic/app/ridehailing/simple/SimpleRideHailingServiceApp.java
@@ -1,41 +1,41 @@
-package com.dcaiti.mosaic.app.ridehailing.simple;
+// package com.dcaiti.mosaic.app.ridehailing.simple;
 
-import com.dcaiti.mosaic.app.ridehailing.AbstractRideHailingServiceApp;
-import com.dcaiti.mosaic.app.ridehailing.server.Ride;
-import com.dcaiti.mosaic.app.ridehailing.server.RideProvider;
-import com.dcaiti.mosaic.app.ridehailing.server.VehicleStatus;
+// import com.dcaiti.mosaic.app.ridehailing.AbstractRideHailingServiceApp;
+// import com.dcaiti.mosaic.app.ridehailing.server.Ride;
+// import com.dcaiti.mosaic.app.ridehailing.server.RideProvider;
+// import com.dcaiti.mosaic.app.ridehailing.server.VehicleStatus;
 
-public class SimpleRideHailingServiceApp extends AbstractRideHailingServiceApp<CSimpleRideHailingServiceApp> {
+// public class SimpleRideHailingServiceApp extends AbstractRideHailingServiceApp<CSimpleRideHailingServiceApp> {
 
-    public SimpleRideHailingServiceApp() {
-        super(CSimpleRideHailingServiceApp.class);
-    }
+//     public SimpleRideHailingServiceApp() {
+//         super(CSimpleRideHailingServiceApp.class);
+//     }
 
-    // Predefined ride orders in a file
-    // A better solution would be converting ride orders into a data stream
-    @Override
-    protected RideProvider createRideBookingProvider() {
-        return new SimpleRideProvider(getConfiguration().rideOrders);
-    }
+//     // Predefined ride orders in a file
+//     // A better solution would be converting ride orders into a data stream
+//     @Override
+//     protected RideProvider createRideBookingProvider() {
+//         return new SimpleRideProvider(getConfiguration().rideOrders);
+//     }
 
-    @Override
-    protected String chooseTaxi(Ride booking) {
-        // simple dispatching: choose first free vehicle in list
-        for (VehicleStatus vehicle : registeredVehicles.values()) {
-            if (vehicle.getCurrentRide() == null) {
-                return vehicle.getVehicleId();
-            }
-        }
-        return null;
-    }
+//     @Override
+//     protected String chooseTaxi(Ride booking) {
+//         // simple dispatching: choose first free vehicle in list
+//         for (VehicleStatus vehicle : registeredVehicles.values()) {
+//             if (vehicle.getCurrentRide() == null) {
+//                 return vehicle.getVehicleId();
+//             }
+//         }
+//         return null;
+//     }
 
-    @Override
-    protected void onVehicleRidePickup(Ride booking) {
+//     @Override
+//     protected void onVehicleRidePickup(Ride booking) {
 
-    }
+//     }
 
-    @Override
-    protected void onVehicleRideDropoff(Ride booking) {
+//     @Override
+//     protected void onVehicleRideDropoff(Ride booking) {
 
-    }
-}
+//     }
+// }
--- src/main/java/com/dcaiti/mosaic/app/ridehailing/simple/SimpleRideHailingVehicleApp.java
+++ src/main/java/com/dcaiti/mosaic/app/ridehailing/simple/SimpleRideHailingVehicleApp.java
@@ -1,27 +1,27 @@
-package com.dcaiti.mosaic.app.ridehailing.simple;
+// package com.dcaiti.mosaic.app.ridehailing.simple;
 
-import com.dcaiti.mosaic.app.ridehailing.AbstractRideHailingVehicleApp;
-import com.dcaiti.mosaic.app.ridehailing.config.CAbstractRideHailingVehicleApp;
-import com.dcaiti.mosaic.app.ridehailing.server.Ride;
+// import com.dcaiti.mosaic.app.ridehailing.AbstractRideHailingVehicleApp;
+// import com.dcaiti.mosaic.app.ridehailing.config.CAbstractRideHailingVehicleApp;
+// import com.dcaiti.mosaic.app.ridehailing.server.Ride;
 
-public class SimpleRideHailingVehicleApp extends AbstractRideHailingVehicleApp<CAbstractRideHailingVehicleApp> {
+// public class SimpleRideHailingVehicleApp extends AbstractRideHailingVehicleApp<CAbstractRideHailingVehicleApp> {
 
-    public SimpleRideHailingVehicleApp() {
-        super(CAbstractRideHailingVehicleApp.class);
-    }
+//     public SimpleRideHailingVehicleApp() {
+//         super(CAbstractRideHailingVehicleApp.class);
+//     }
 
-    @Override
-    protected void onAcceptRide(Ride ride) {
+//     @Override
+//     protected void onAcceptRide(Ride ride) {
 
-    }
+//     }
 
-    @Override
-    protected void onPickup(Ride ride) {
+//     @Override
+//     protected void onPickup(Ride ride) {
 
-    }
+//     }
 
-    @Override
-    protected void onDropOff(Ride ride) {
+//     @Override
+//     protected void onDropOff(Ride ride) {
 
-    }
-}
+//     }
+// }
