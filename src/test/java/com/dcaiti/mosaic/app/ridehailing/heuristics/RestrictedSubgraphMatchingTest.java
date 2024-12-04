f08774c3c84fc927b40a94344564e756231dfbf7
Kevin Nguyen <k.nguyen.2@campus.tu-berlin.de>
2024-12-04 12:36:20 +0100

    Add distance test

---
+++ src/test/java/com/dcaiti/mosaic/app/ridehailing/heuristics/RestrictedSubgraphMatchingTest.java
@@ -0,0 +1,34 @@
+package com.dcaiti.mosaic.app.ridehailing.heuristics;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+import org.eclipse.mosaic.lib.geo.CartesianPoint;
+import org.eclipse.mosaic.lib.geo.MutableCartesianPoint;
+
+class RestrictedSubgraphMatchingTest {
+
+    @Test
+    void testDistance() {
+        CartesianPoint point1 = new MutableCartesianPoint(0, 0, 0);
+        CartesianPoint point2 = new MutableCartesianPoint(3, 4, 0);
+        double expectedDistance = 5.0;
+
+        double actualDistance = RestrictedSubgraphMatching.distance(point1, point2);
+
+        assertEquals(expectedDistance, actualDistance, 0.0001, "The distance between the points should be 5.0");
+    }
+
+    @Test
+    void testDistanceWithNegativeCoordinates() {
+        CartesianPoint point1 = new MutableCartesianPoint(-3, -4, 0);
+        CartesianPoint point2 = new MutableCartesianPoint(3, 4, 0);
+        double expectedDistance = 10.0;
+
+        double actualDistance = RestrictedSubgraphMatching.distance(point1, point2);
+
+        assertEquals(expectedDistance, actualDistance, 0.0001, "The distance between the points should be 10.0");
+    }
+}
