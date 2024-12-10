085f821ea7539f2aa41c0bf92f0bee9476297733
Kevin Nguyen <k.nguyen.2@campus.tu-berlin.de>
2024-12-10 12:06:16 +0100

    Delete ride hailing files

--- src/main/java/com/dcaiti/mosaic/app/ridehailing/config/CAbstractRideHailingVehicleApp.java
+++
@@ -1,9 +0,0 @@
-package com.dcaiti.mosaic.app.ridehailing.config;
-
-public class CAbstractRideHailingVehicleApp {
-
-    /**
-     * The name of the server the {@link com.dcaiti.mosaic.app.ridehailing.AbstractRideHailingServiceApp} is running on.
-     */
-    public String serverName = "server_0";
-}
